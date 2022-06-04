8a353ae4f3bcf89c0344924840a6d498f97b4a16
cihat <cihat02020202@gmail.com>
2022-06-04 21:50:17 +0300

    Change users file name to tweet.js

---
+++ src/routes/tweet.js
@@ -0,0 +1,23 @@
+const usersController = require('../controllers/tweet.js')
+
+const router = require('express').Router()
+
+router.post('/', usersController.createTweet)
+
+// router.get('/', usersController.getUsers)
+
+// router.post('/', usersController.postUser)
+
+// router.delete('/:userId', usersController.deleteUser)
+
+// router.get('/:userId', usersController.getUser)
+
+// router.patch('/:userId/following', usersController.follow)
+
+// router.patch('/:userId', usersController.updateName)
+
+// router.patch('/:userId/like', usersController.like)
+
+// router.patch('/:userId/retweet', usersController.retweet)
+
+module.exports = router
