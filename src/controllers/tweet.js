cd1807b745fe483b968486c9b2c54c05008c5a20
cihat <cihat02020202@gmail.com>
2022-06-06 02:54:03 +0300

    Add `Tweet, User` controllers

--- src/controllers/tweet.js
+++ src/controllers/tweet.js
@@ -1,77 +1,147 @@
-const user = require('../models/user')
-const { userService, tweetService } = require('../services')
+const Tweet = require('../models/tweet')
+const { tweetService } = require('../services')
+const Validator = require('async-validator').default
 
-exports.getUsers = async (req, res) => {
-  const users = await userService.load()
+exports.createTweet = async (req, res, next) => {
+  const descriptor = {
+    content: [
+      { requied: true, message: 'Tweet message is required' },
+      {
+        min: 1,
+        message: 'Tweet message should be between 1 and 140 characters'
+      },
+      {
+        max: 140,
+        message: 'Tweet message should be between 1 and 140 characters'
+      }
+      // {
+      //   pattern: /^[a-zA-Z0-9_]+$/,
+      //   message: 'Tweet message should be alphanumeric'
+      // },
+      // { pattern: /^\S+$/, message: 'Tweet message should not include spaces' }
+    ],
+    author: [{ required: true, message: 'Author is required' }]
+  }
 
-  const type = req.query.type || 'json'
+  const validator = new Validator(descriptor)
 
-  if (type == 'json') res.send(users)
-  else res.render('users', { users })
+  console.log('user', req.user)
+
+  const tweetRequest = {
+    content: req.body.content,
+    author: req.user._id
+  }
+
+  try {
+    await validator.validate(tweetRequest)
+  } catch ({ errors }) {
+    return next({ message: errors.map((e) => e.message).join('') })
+  }
+
+  let newTweet = new Tweet(tweetRequest)
+
+  try {
+    newTweet.save()
+
+    req.user.tweets.push(newTweet)
+    await req.user.save()
+
+    res.status(201).send(newTweet)
+  } catch (error) {
+    return next(error)
+  }
 }
 
-exports.postUser = async (req, res) => {
-  const user = await userService.insert(req.body)
-
-  res.send(user)
+exports.getTweets = async (req, res, next) => {
+  try {
+    const tweets = await tweetService.findAll()
+    res.status(200).send(tweets)
+  } catch (error) {
+    return next(error)
+  }
 }
 
-exports.deleteUser = async (req, res) => {
-  await userService.removeBy('_id', req.params.userId)
-
-  res.send('OK')
+exports.getTweet = async (req, res, next) => {
+  try {
+    const tweet = await tweetService.find(req.params.tweetId)
+    res.status(200).send(tweet)
+  } catch (error) {
+    return next(error)
+  }
 }
 
-exports.getUser = async (req, res) => {
-  const user = await userService.find(req.params.userId)
-  if (!user) return res.status(404).send('Cannot find user')
-  const type = req.query.type || 'json'
+exports.likeTweet = async (req, res, next) => {
+  try {
+    const tweet = await tweetService.likeTweet(req.params.tweetId, req.user._id)
 
-  if (type == 'json') res.send(user)
-  else res.render('user', { user })
+    res.status(200).send(tweet)
+  } catch (error) {
+    return next(error)
+  }
 }
 
-exports.createTweet = async (req, res) => {
-  const { userId } = req.params
-  const { body } = req.body
+// exports.getUsers = async (req, res) => {
+//   const users = await userService.load()
 
-  const tweet = await tweetService.tweet(userId, body)
+//   const type = req.query.type || 'json'
 
-  res.send(tweet)
-}
+//   if (type == 'json') res.send(users)
+//   else res.render('users', { users })
+// }
 
-exports.updateName = async (req, res) => {
-  const { userId } = req.params
-  const { name } = req.body
+// exports.postUser = async (req, res) => {
+//   const user = await userService.insert(req.body)
 
-  const updateName = await userService.updateName(userId, name)
+//   res.send(user)
+// }
 
-  res.send(updateName)
-}
+// exports.deleteUser = async (req, res) => {
+//   await userService.removeBy('_id', req.params.userId)
 
-exports.follow = async (req, res) => {
-  const { userId } = req.params
-  const { userToFollowId } = req.body
+//   res.send('OK')
+// }
 
-  const users = await userService.follow(userId, userToFollowId)
+// exports.getUser = async (req, res) => {
+//   const user = await userService.find(req.params.userId)
+//   if (!user) return res.status(404).send('Cannot find user')
+//   const type = req.query.type || 'json'
 
-  res.send(users)
-}
+//   if (type == 'json') res.send(user)
+//   else res.render('user', { user })
+// }
 
-exports.like = async (req, res) => {
-  const { userId } = req.params
-  const { likeTweetId } = req.body
+// exports.updateName = async (req, res) => {
+//   const { userId } = req.params
+//   const { name } = req.body
 
-  const user = await tweetService.like(userId, likeTweetId)
+//   const updateName = await userService.updateName(userId, name)
 
-  res.send(user)
-}
+//   res.send(updateName)
+// }
 
-exports.retweet = async (req, res) => {
-  const { userId } = req.params
-  const { retweetId } = req.body
+// exports.follow = async (req, res) => {
+//   const { userId } = req.params
+//   const { userToFollowId } = req.body
 
-  const user = await tweetService.retweet(userId, retweetId)
+//   const users = await userService.follow(userId, userToFollowId)
 
-  res.send(user)
-}
+//   res.send(users)
+// }
+
+// exports.like = async (req, res) => {
+//   const { userId } = req.params
+//   const { likeTweetId } = req.body
+
+//   const user = await tweetService.like(userId, likeTweetId)
+
+//   res.send(user)
+// }
+
+// exports.retweet = async (req, res) => {
+//   const { userId } = req.params
+//   const { retweetId } = req.body
+
+//   const user = await tweetService.retweet(userId, retweetId)
+
+//   res.send(user)
+// }
